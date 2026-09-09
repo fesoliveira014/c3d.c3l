9cc56e17f9fa9a789c39e422cc101c3d742022fa
fesoliveira014 <fesoliveira@outlook.com>
2026-09-08 23:03:37 -0600

    style(gui): separate logical blocks and expand signatures

--- shaders/imgui.vert.glsl
+++ shaders/imgui.vert.glsl
@@ -17,8 +17,10 @@ layout(location = 1) out vec4 v_color;
 void main() {
     GuiVertexRoot root = GuiVertexRoot(pc.vertex_root_gpu);
     GuiVertexGpu vertex = GuiVertices(root.vertices).values[gl_VertexIndex];
+
     vec2 position = vec2(vertex.position_x, vertex.position_y);
     gl_Position = vec4(position * root.scale + root.translate, 0.0, 1.0);
+
     v_uv = vec2(vertex.uv_x, vertex.uv_y);
     v_color = unpackUnorm4x8(vertex.rgba);
 }
